14b91d4baa1e72a07b8a8f3613a98c011551e52a
AntonAhlm <113014145+AntonAhlm@users.noreply.github.com>
2022-10-19 10:20:40 +0200

    Update Fibonacci.py

--- Fibonacci/Fibonacci.py
+++ Fibonacci/Fibonacci.py
@@ -1,42 +1,33 @@
 a=0
 tal1=1
 tal2=0
-
-while a<40:
-    while a<9: 
+space1=str("")
+space2=str("")
+while a<40: 
        a+=1
+       if a<10:
+           space1=str(" ")
+       else:
+           space1=str("")
+       if a<18:
+           space2=str("\t")
+       else:
+           space2=str("")
+ 
+
        if a % 2 == 0:
            if tal2>2 or tal1>2: 
-               print("Tal",a," :",tal1, "\t\t Kvot:",tal1/tal2 )
+               print("Tal",a,space1,":",tal1,space2, "\t\t Kvot:",tal1/tal2 )
                tal2+=tal1 
            else: 
-               print("Tal",a," :",tal1 )
+               print("Tal",a,space1,":",tal1 )
                tal2+=tal1 
 
        else :
            if tal2>2 or tal1>2: 
-               print("Tal",a," :",tal2, "\t\t Kvot:",tal2/tal1)  
+               print("Tal",a,space1,":",tal2,space2, "\t\t Kvot:",tal2/tal1)  
                tal1+=tal2
            else:
-               print("Tal",a," :",tal2,) 
+               print("Tal",a,space1,":",tal2,) 
                tal1+=tal2
-           
-
-
-    while a>=9 and a<26:  #Nästa stycke blev förskjutet en \t eftersom det var för långa tal. Lade till så att resten av programmet också är +1 \t och är i en rak linje
-         a+=1
-         if a % 2 == 0:
-            print("Tal",a,":",tal1, "\t\t Kvot:",tal1/tal2  )
-            tal2+=tal1
-         else :
-            print("Tal",a,":",tal2, "\t\t Kvot:",tal2/tal1 )
-            tal1+=tal2
-
-
-    a+=1
-    if a % 2 == 0:
-        print("Tal",a,":",tal1, "\t Kvot:",tal1/tal2  )
-        tal2+=tal1
-    else :
-        print("Tal",a,":",tal2, "\t Kvot:",tal2/tal1 )
-        tal1+=tal2
+      
